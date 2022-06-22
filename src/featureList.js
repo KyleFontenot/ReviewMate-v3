20684c9035eb310552993e868a2c2c8bc003b4f6
Kyle Fontenot <kylewilliamfontenot@gmail.com>
2022-06-21 23:17:15 -0700

    Started adding in information of featureList

--- src/featureList.js
+++ src/featureList.js
@@ -13,7 +13,7 @@
 };
 
 class Module {
-	constructor(title, short, long, img, tiers, bullets, ) {
+	constructor(title, short, long, img, tiers, bullets ) {
 		this.title = title;
 		this.slug = formatSlug(title);
 		this.short = short;
@@ -23,6 +23,7 @@ class Module {
 		this.essentials = tiers[0];
 		this.pro = tiers[1];
 		this.enterprise = tiers[2];
+		// bullets should be an object type
 		this.bullets = bullets;
 		this.commontraits = false;
 		this.tag = this.title
@@ -47,10 +48,13 @@ const featureList = {
 			],
 			{ src: "/src/images/photos/rodnae.jpg", alt: "Doctors meeting together" },
 			[true, true, true],
-			[
-				"Identification on the screen of codes with MCC/CC/HAC and CMS-HCC values.",
-				"Support for Profee audit within the IPPS module.",
-			]
+			{
+				header: "In addition, our IPPS module features:",
+				bullets: [
+					"Identification on the screen of codes with MCC/CC/HAC and CMS-HCC values.",
+					"Support for Profee audit within the IPPS module.",
+				],
+			}
 		),
 		new AuditingModule(
 			"Outpatient",
@@ -70,18 +74,25 @@ const featureList = {
 		new AuditingModule(
 			"Profee",
 			"Physician Fee schedule and/or Physician office auditing is among the other core auditing standards.",
-			["Long paragraph"],
+			[
+				"ProFee, or professional fee, refers to coding and billing the physician side of a patient encounter. ProFee coding covers the work performed by the provider and the reimbursement they will receive for the medical services performed.",
+				"Becuase Profee billing directly affects providers, it can be more strict than other types of accounts. Becuase of the very narrow margin for error as an auditor, ReviewMate is prepared in addressing the exact punctuality that you need.",
+			],
 			{
 				src: "/src/images/photos/polina.jpg",
 				alt: "Female doctor analyzing a chart",
 			},
 			[true, true, true]
-			// add bulletopints here
+			// add bulletpoints here
 		),
 		new AuditingModule(
 			"ASC",
 			"Free-standing ambulatory surgery center is a first-class citizen to the standard auditing capabilities.",
-			["Long paragraph"],
+			// not in the business of providing office visits, laboratory services, diagnostic tests,
+			[
+				"Coding for Ambulatory Surgery Centers is a specialty unto itself. It is a facility service, but Medicare requires ASC’s to send their bills to the professional fee payers through the facility. There is a whole different set of regulations and bundling edits to use for ASCs, mainly pertaining to Medicare.",
+				"Given the more strict nature of ASC, ReviewMate excels in maintaining the punctuality needed using it's standard features of communication and transparency.",
+			],
 			{
 				src: "/src/images/photos/ambulance.jpg",
 				alt: "Nurses loading a patient into a gurney",
@@ -91,8 +102,12 @@ const featureList = {
 		),
 		new AuditingModule(
 			"CMS-HCC",
-			"CMS(centers for medical service), Hierarchical Condition Categories, Risk Adjustment.",
-			["Long paragraph"],
+			"In a risk-adjustment and value-based payment system, HCC is vital in modern health information systems. ReviewMate pushes important statistics like performance targets and averages to keep you on top of your auditing.",
+			[
+				"Hierarchical condition category (HCC) coding is a risk-adjustment model originally designed to estimate future health care costs for patients. HCC coding helps communicate patient complexity and paint a picture of the whole patient. In addition to helping predict health care resource utilization, RAF scores are used to risk adjust quality and cost metrics. By accounting for differences in patient complexity, quality and cost performance can be more appropriately measured.",
+				"CMS uses two models: The first, CMS-HCC is the model used to pay MAOs. The second model was developed after the passage of the Affordable Care Act to pay health insurers in the ACA marketplace. This second model includes categories for infants, children, and all age adults, and includes obstetrical diagnosis codes for high risk OB care. ",
+				"ReviewMate pushes important metrics like performance targets and averages to keep you proactive on audits.",
+			],
 			{
 				src: "/src/images/photos/anna-shvets.jpg",
 				alt: "Medical machines in a patient's room",
@@ -104,23 +119,29 @@ const featureList = {
 	reporting: [
 		new Module(
 			"Accuracy Manager",
-			"Choose from a range of parameters and gather custom accuracy rates.",
+			"Choose from a range of parameters and gather custom accuracy rates. Arrange a group, and pull the findings you need exactly.",
 			[
-				"Allows the slicing and dicing your data for aggregating custom accuracy rates.  View accuracy rates by different entities, groups, or date ranges.",
-				"Allows the slicing and dicing your data for aggregating custom accuracy rates.  View accuracy rates by different entities, groups, or date ranges.",
+				"From different record account types, to calculation method on querying findings, ReviewMate is an entirely comprehensive tool for pinpointing results. Not only is it a great tool for auditors to surgically find neccessary tools, but the Accuracy Manager connects directly with the rest of Reviewmate, such as tools like: Reporting Audit The Auditor, and Task Workflows.",
 			],
 			{
 				src: "/src/images/photos/lukas.jpg",
 				alt: "Statistics on multiple sheets of paper",
 			},
-			[true, true, true]
+			[true, true, true],
+			{
+				header: "Common parameters for using Accuracy Manager",
+				bullets: [
+					"Identification on the screen of codes with MCC/CC/HAC and CMS-HCC values.",
+					"Support for Profee audit within the IPPS module.",
+				],
+			}
 			// add bulletpoints here
 		),
 		new Module(
 			"Code Finder",
-			"Reference specific code instances historically system-wide. Expand when codes were used previously etc.",
+			"Reference specific code instances historically system-wide. Index historically used codes.",
 			[
-				"Allows to slice and dice your data for  View accuracy rates for different entities, groups, dates, by slicing result findings.",
+				"An integrated tool with Reviewmate is Code Finder, that directly correlates submitted codes with historical findings of the same code. Instead of querying searches by coder, tasks, facility, or any other parameter, you can query by particular code instances historically.",
 			],
 			{
 				src: "/src/images/photos/tima.jpg",
@@ -297,14 +318,20 @@ const featureList = {
 			"ReviewMate Insights (Benchmarking)",
 			"Benchmark and compare performance against other ReviewMate users. Offers advice, community and insight within local scopes, global scopes, or 'millions of records within our community'. Exposes common coding errors that integrate with your Risk Manager.",
 			[""],
-			{ src: "/src/images/photos/groupidea.jpg", alt: "Group of happy diverse business people standing in studio and holding glowing light" },
+			{
+				src: "/src/images/photos/groupidea.jpg",
+				alt: "Group of happy diverse business people standing in studio and holding glowing light",
+			},
 			[false, true, true]
 		),
 		new Module(
 			"Selector Tools",
 			"Expanded utilities for querying your universe of data. Explicit parameters for querying in addition to a randomization query tool.",
 			[""],
-			{ src: "/src/images/photos/selection.jpg", alt: "Customer group circled" },
+			{
+				src: "/src/images/photos/selection.jpg",
+				alt: "Customer group circled",
+			},
 			[true, true, true]
 		),
 
@@ -312,7 +339,10 @@ const featureList = {
 			"Customizable Tasks",
 			"Customizable task responses allow all users to focus on pertinent issues. Field or view verbose responses based on user preferences.",
 			[""],
-			{ src: "/src/images/photos/postitnotes.jpg", alt: "Three differentl colored Post-it notes on a window" },
+			{
+				src: "/src/images/photos/postitnotes.jpg",
+				alt: "Three differentl colored Post-it notes on a window",
+			},
 			[true, true, true]
 		),
 
@@ -320,7 +350,10 @@ const featureList = {
 			"Risk Manager",
 			"Leverage artificial intelligence and historical findings to monitor and stay on-top of potential risks.",
 			[""],
-			{ src: "/src/images/photos/riskaversion.jpg", alt: "Partial view of risk manager blocking toppling dominoes" },
+			{
+				src: "/src/images/photos/riskaversion.jpg",
+				alt: "Partial view of risk manager blocking toppling dominoes",
+			},
 			[false, false, true]
 		),
 
@@ -328,14 +361,20 @@ const featureList = {
 			"Record Tracker",
 			"View full narratives of records in one integrated place by using timelines within each record.",
 			[""],
-			{ src: "/src/images/photos/recordtracker.jpg", alt: "Woman and physician meeting over a medical record" },
+			{
+				src: "/src/images/photos/recordtracker.jpg",
+				alt: "Woman and physician meeting over a medical record",
+			},
 			[false, false, true]
 		),
 		new Module(
 			"Retrospective Physician Queries",
 			"Intuitively analyze and report coders' physician queries on compliance, appropriateness, and efficiency.",
 			[""],
-			{ src: "/src/images/photos/physicianquery.jpg", alt: "Doctor greeting and collaborating with businessman" },
+			{
+				src: "/src/images/photos/physicianquery.jpg",
+				alt: "Doctor greeting and collaborating with businessman",
+			},
 			[false, true, true]
 		),
 
@@ -343,7 +382,10 @@ const featureList = {
 			"CDI Communication",
 			"Integrate communication with any first-party or third-party CDI team.",
 			[""],
-			{ src: "/src/images/photos/CDIcommunication.jpg", alt: "Young woman scrutinizing charts on a monitor" },
+			{
+				src: "/src/images/photos/CDIcommunication.jpg",
+				alt: "Young woman scrutinizing charts on a monitor",
+			},
 			[false, false, true]
 		),
 	],
