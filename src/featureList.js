13b78f195c29d9b341e8c2d828e0bbbc82c357a6
Kyle Fontenot <kwfonten@asu.edu>
2022-06-25 08:48:19 -0700

    Adding in some more featureList

--- src/featureList.js
+++ src/featureList.js
@@ -121,7 +121,7 @@ const featureList = {
 			"Accuracy Manager",
 			"Choose from a range of parameters and gather custom accuracy rates. Arrange a group, and pull the findings you need exactly.",
 			[
-				"From different record account types, to calculation method on querying findings, ReviewMate is an entirely comprehensive tool for pinpointing results. Not only is it a great tool for auditors to surgically find neccessary tools, but the Accuracy Manager connects directly with the rest of Reviewmate, such as tools like: Reporting Audit The Auditor, and Task Workflows.",
+				"ReviewMate is an entirely comprehensive tool for summarizing results. Use the Accuracy Manager for querying any type of account or task using a variety of parameters to choose from. These results can be used for tracking live progress, or for export into a type of report. The Accuracy Manager is a great tool for auditors to surgically find neccessary findings, but the Accuracy Manager connects directly with the rest of ReviewMate tools like: Reporting, Audit The Auditor, and Task Workflows.",
 			],
 			{
 				src: "/src/images/photos/lukas.jpg",
@@ -129,10 +129,10 @@ const featureList = {
 			},
 			[true, true, true],
 			{
+				// !!!    
 				header: "Common parameters for using Accuracy Manager",
 				bullets: [
-					"Identification on the screen of codes with MCC/CC/HAC and CMS-HCC values.",
-					"Support for Profee audit within the IPPS module.",
+					""
 				],
 			}
 			// add bulletpoints here
@@ -141,7 +141,7 @@ const featureList = {
 			"Code Finder",
 			"Reference specific code instances historically system-wide. Index historically used codes.",
 			[
-				"An integrated tool with Reviewmate is Code Finder, that directly correlates submitted codes with historical findings of the same code. Instead of querying searches by coder, tasks, facility, or any other parameter, you can query by particular code instances historically.",
+				"An integrated tool with Reviewmate is Code Finder. It directly correlates submitted codes with historical findings of the same code. Instead of querying searches by coder, tasks, facility, or any other parameter, you can query by particular code instances historically, providing a shortcut to particular instances and providing specific insight.",
 			],
 			{
 				src: "/src/images/photos/tima.jpg",
@@ -154,7 +154,8 @@ const featureList = {
 			"Dashboards",
 			"Customizable dashboards for organizing the data you need. Underline pertinent workflows for auditors, administrators, or create client dashboards for consuming results.",
 			[
-				"Allows to slice and dice your data for  View accuracy rates for different entities, groups, dates, by slicing result findings.",
+				"From common data formations and models in prefabricated arrangements, dashboards deliver important information succinctly. All dashboards feature pie charts, graphs and tables that can be arranged and customized for each user's particular use.",
+				"The most common setup is for an auditor to track progress on many different tasks simultaneously, allowing him or her to visually assess one's progress. Additionally, dashboards can be setup for other users such as clients and coders to be able to communicate the entire auditing process more efficiently. "
 			],
 			{
 				src: "/src/images/photos/cottonbro.jpg",
@@ -167,7 +168,9 @@ const featureList = {
 			"Summary Templates",
 			"Unlimited amount of customizable templates for your firm. From branding to typography, keep your reports consistent seamlessly.",
 			[
-				"Unlimited amount of customizable templates for your firm. From branding to typography, keep your reports consistent seamlessly.",
+				"Our reporting offer many ways of customizing reports, but templating these reports is where users find great value. Users can choose from common pre-determined templates, or customize your templates to fit your firm's needs.",
+				// !!! Would like some specific pre-made templates as suggestions
+				""
 			],
 			{
 				src: "/src/images/photos/artem.jpg",
@@ -180,7 +183,9 @@ const featureList = {
 			"Coder Report Cards",
 			"Easily digestible reports on accuracy with exact comment notes from all associated reviewers. Distributed coder login credentials allow for direct viewable transcripts for accoutability.",
 			[
-				"Unlimited amount of customizable templates for your firm. From branding to typography, keep your reports consistent seamlessly.",
+				"Reports are primarily meant for clients, but offering to involve the relevant coders in audit findings is an excellent way to expand communication and accountability for all parties. Coder Report Cards are a smaller, more concise, and automatic version of reports meant specifically coders.",
+				"When auditors allow, coders can sign into ReviewMate using their own credentials for viewing the coder's own performance. ReviewMate empowers both coder and auditor by providing accountability and even offering tools for following up with assessments."
+				// !!! Mention something about    "Feeback from coder users is really great."
 			],
 			{
 				src: "/src/images/photos/kindel.jpg",
@@ -193,7 +198,8 @@ const featureList = {
 			"Exportability",
 			"All reports can be exported into PDFs, Excel spreadsheets, Word documents.",
 			[
-				"Unlimited amount of customizable templates for your firm. From branding to typography, keep your reports consistent seamlessly.",
+				"After aggregating your results into a report of your choosing, ReviewMate offers all standard ways of exporting your report into usable documents. The auditing industry has many interoperability challenges, so these standard document formats are treated as first-class citizens within ReviewMate.",
+				// !!! re there other formats that I'm missing here?
 			],
 			{
 				src: "/src/images/photos/pixabay.jpg",
@@ -207,7 +213,10 @@ const featureList = {
 		new Module(
 			"Multifactor Authentication",
 			"Confirm fidelity with more complex sign-on options. Integration with Google Duo.",
-			[""],
+			["ReviewMate treats HIPAA requirements with the utmost importance. Multi-factor Authentication is one way that secures each user's login. By using modern standard email authentication and Google's Duo service, user fidelity is guaranteed.",
+			"Google Duo is available on every app store for mobile devices to track a per-device policy to prove authenticity. Google Duo can be disabled with each user's settings."
+		],
+
 			{
 				src: "/src/images/photos/frederik.jpg",
 				alt: "Cell phone on homescreen",
@@ -217,7 +226,9 @@ const featureList = {
 		new Module(
 			"LDAP / Active Directory",
 			"Connect directly into enterprise LDAP system from within ReviewMate. Master passwords work within ReviewMate for seamless user authentication.",
-			[""],
+			[
+				`A common industry standard for connecting to a hospital or firm's database –or as we call the firm's "universe of data"– is using LDAP or Active Directory. It is a file-sharing tool for networking connections between computers to other computers or resources. ReviewMate can use LDAP or Active Directory natively in the platform so that data is easily to obtain in a secure, point-to-point method.`,
+			],
 			{
 				src: "/src/images/photos/morillo.jpg",
 				alt: "Woman gaining access to server",
@@ -227,7 +238,7 @@ const featureList = {
 		new Module(
 			"VPN Integration",
 			"End-to-end VPN tunneling compatibility with SOC2 certified compliance.",
-			[""],
+			["With many hospitals and institutions, connecting remotely to servers or even to individual computers almost always requires the use of connection to a private VPN. ReviewMate fully supports VPN connections for firectly tunneling connections to the source."],
 			{
 				src: "/src/images/photos/vpn.jpg",
 				alt: "Computer connecting to a VPN service",
@@ -238,7 +249,7 @@ const featureList = {
 
 	analytics: [
 		new Module(
-			"Staffing Analaytics",
+			"Staffing Analytics",
 			"Time tracking and progress transparency, against tasks, findings, accounts. Track goals and accountability with visual charts and dashboard plugins.",
 			[""],
 			{
@@ -251,7 +262,11 @@ const featureList = {
 		new Module(
 			"System Metrics",
 			"Utilize visual graphs and charts to focus on performance-based results. Discover prioritization concerns as they happen.",
-			[""],
+			[
+				"Analyzing coder accuracy is the primary use for accumulating accuracy rates, but ReviewMate offers visual insight on other systematic details across a user's and even a firm's usage. Particularly when working with a team, performance statistics and workload can be viewed at a glance.", 
+				"Components of both dashboards and summaries have many tools to visualize statistical data from findings. We use custom bar charts, line graphs, and pie charts to display any metric that the user sees fit."
+				// !!! Can bars charts and graphs be put into the reports? 
+			],
 			{
 				src: "/src/images/photos/negative-space.jpg",
 				alt: "Laptop showing results of query",
@@ -274,7 +289,8 @@ const featureList = {
 		new Module(
 			"Integration Manager",
 			"Import spreadsheets, universes of data, or any other third-party source of data into a central resource. Manage, maintain, and navigate sources dynamically.",
-			[""],
+			[`For the many ways a user can integrate and receive data from a source such as a "universe of data", ReviewMate allows in interface for configuring all of the users' connections to the data sources. These connections are what we consider integrations. `],
+			// !!! more third-party integrations would be good to mention.
 			{
 				src: "/src/images/photos/sayles.jpg",
 				alt: "Many cords plugged into electronic",
@@ -285,7 +301,8 @@ const featureList = {
 		new Module(
 			"Audit The Auditor",
 			"Review completed audits and reviews by adding  layered documentation. Original data is referential and integrated.",
-			[""],
+			["Every auditor has the ability to interact with already-finished audits. Comments and notes can be attached directly to a specific code, a coder, or for maybe the auditor themselves. By doing this, it creates a layer of communication on top of the rest of the finding's communication shared by coder, auditor, and potentially client.",
+		"This way of stacking communication is similar to modern thread-based messaging that enhances transparency for every user of the chain of interaction."],
 			{
 				src: "/src/images/photos/andrey.jpg",
 				alt: "Midsection of adult examining a printed business summary",
@@ -296,7 +313,10 @@ const featureList = {
 		new Module(
 			"Document Manager",
 			"Share files such as reports, imported spreadsheets, and training materials between different administration levels. HIPAA compliant, and supported by clear version controlling.",
-			[""],
+			["Within ReviewMate, users may import and export documents such as Word documents and Excel spreadsheets right into the system. Document Manager is the overview tool for organizing your documents from all sources."
+			// !!! I dont know what othe features to talk about.
+		],
+
 			{
 				src: "/src/images/photos/documentmanager.jpg",
 				alt: "Man examining a document",
@@ -307,7 +327,11 @@ const featureList = {
 		new Module(
 			"Education Tracking",
 			"Directly assign training materials from either custom uploaded training resources such as videos, PDFs, and powerpoints, or use integrated nThrive's training modules. Create and assign quizzes",
-			[""],
+			// !!! quizzes? ?
+			[
+				"While auditors review coders' evaluations, auditors have the option of suggesting training materials. Communication with the coder is optional though training suggestions –especially for common coding mistakes– is a very impactful feature that intuitively helps coder, auditor, and client alike.",
+				"Training materials are provided by nThrive, a developed service that provides AHIMA compliant tutorials and relevant articles for assisting in billing education. All codes within ReviewMate are indexable toward training materials, so suggesting training materials is a matter of blicking a button."
+			],
 			{
 				src: "/src/images/photos/goodluz.jpg",
 				alt: "Young adults using touchpad",
@@ -317,7 +341,7 @@ const featureList = {
 		new Module(
 			"ReviewMate Insights (Benchmarking)",
 			"Benchmark and compare performance against other ReviewMate users. Offers advice, community and insight within local scopes, global scopes, or 'millions of records within our community'. Exposes common coding errors that integrate with your Risk Manager.",
-			[""],
+			["ReviewMate Insights is an community of all ReviewMate users, assembled on the intent of improving common issues. With the community, users can leverage insight for addressing topics such as most problematic coding errors. These types of insight are viewable to users, but they also contribute into artificial intelligence within ReviewMate to enhance user experience. "],
 			{
 				src: "/src/images/photos/groupidea.jpg",
 				alt: "Group of happy diverse business people standing in studio and holding glowing light",
