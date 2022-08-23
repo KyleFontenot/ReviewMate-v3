8b92f0a943a92a06e93a2cf579227d0787d1460c
Kyle Fontenot <kylewilliamfontenot@gmail.com>
2022-08-23 10:56:02 -0700

    Fixed small error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import solid from "@astrojs/solid-js";
-import compress from 'vite-plugin-compress';
+// import compress from 'vite-plugin-compress';
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,9 +14,6 @@ export default defineConfig({
   outDir: "./dist",
   publicDir: "./public",
   // trailingSlash: "always",
-  site: "https://main--deft-cuchufli-bfd7e3.netlify.app/",
-  integrations: [astroImageTools, solid(), sitemap(), image()],
-  vite: {
-    plugins: [compress]
-  }
+  site: "https://review-mate.com/",
+  integrations: [astroImageTools, solid(), sitemap(), image(), compress()],
 });
