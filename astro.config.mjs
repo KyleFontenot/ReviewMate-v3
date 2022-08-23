2eca45647b31b67a128e55ff1f7b779eeabff968
Kyle Fontenot <kylewilliamfontenot@gmail.com>
2022-08-23 11:29:24 -0700

    Trying to revert astro version for astro-imagetools compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 import { defineConfig } from "astro/config";
-import { astroImageTools } from "astro-imagetools";
 import solid from "@astrojs/solid-js";
-import compress from 'vite-plugin-compress';
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
+import { astroImageTools } from "astro-imagetools";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	root: ".",
-	srcDir: "./src",
-	outDir: "./dist",
-	publicDir: "./public",
-	// trailingSlash: "always",
-	site: "https://review-mate.com/",
-	integrations: [astroImageTools, solid(), sitemap(), image()],
-	vite: {
-		plugins: [compress],
-	},
+  root: ".",
+  srcDir: "./src",
+  outDir: "./dist",
+  publicDir: "./public",
+  // trailingSlash: "always",
+  site: "https://review-mate.com/",
+  integrations: [astroImageTools, solid(), sitemap(),  compress()],
 });
