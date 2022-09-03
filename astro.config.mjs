ededcaea32d706b8c3dbb110ecd21809a1dd5b95
Kyle Fontenot <kwfonten@asu.edu>
2022-09-03 06:55:14 -0700

    Beginning testing and creating CHangeTracker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
 import solidJs from "@astrojs/solid-js";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	root: ".",
-	srcDir: "./src",
-	outDir: "./dist",
-	publicDir: "./public",
-	site: "https://review-mate.com/",
-	integrations: [astroImageTools, solidJs(), sitemap()],
+  root: ".",
+  srcDir: "./src",
+  outDir: "./dist",
+  publicDir: "./public",
+  site: "https://review-mate.com/",
+  integrations: [astroImageTools, solidJs(), sitemap(), compress()]
 });
