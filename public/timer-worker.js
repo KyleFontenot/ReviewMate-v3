603c276e0a30bced5b186e3c1321837b1342fa73
Kyle Fontenot <kwfonten@asu.edu>
2022-09-26 07:04:38 -0700

    Progress on carousel

---
+++ public/timer-worker.js
@@ -0,0 +1,10 @@
+// let timer = 0;
+// let timerinterval = setInterval(() => {
+//   timer++
+// }, 200)
+// self.onmessage = function(e){
+//   if (e.data.message === "START" || e.data.message === "RESET") {
+//     timer = 0;
+// 	}
+
+// }
